0b91faa71bd94b8fb5209db5c5129fcd4193e46f
Ron Hughes <rondonsonmusic@gmail.com>
2023-02-05 19:41:23 -0500

    updated score logic

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-const uri = 'mongodb+srv://rondonson:clockhit@cluster0.ymn1ivm.mongodb.net/?retryWrites=true&w=majority'
+const uri = process.env.DATABASE_URI;
 
+//middleware
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
-
+//connect to database
 MongoClient.connect(uri, {
     useUnifiedTopology: true
 })
@@ -19,9 +21,6 @@ MongoClient.connect(uri, {
     const scores = db.collection('score')
 
 
-
-
-
 // ROUTES AND REQUESTS
 
 app.get('/', (req, res) => {
@@ -29,10 +28,43 @@ app.get('/', (req, res) => {
 })
 
 app.post('/flip', (req, res) => {
-console.log(req.body)
+    function flipCoin() {
+        const choices = ['heads', 'tails']
+        const randomSide = Math.floor(Math.random() * choices.length)
+        return choices[randomSide]      
+}   
+let name = req.body.playerName
 
+if (flipCoin() === req.body.result) {
+    // TODO: add POST to score database if win
+    //  res.json('You Win')
+    // console.log(`${name} wins`)
+    scores.updateOne(
+        { name: name },
+        { $inc: { wins: 1}},
+        { upsert: true },
+        function(err, result) {
+            if (err) throw err
+        }        
+    );
+    res.json('You Win')
+     return;
+} 
 
-})
+else {
+    console.log(`${name} loses`);
+    scores.updateOne(
+      { name: name },
+      { $inc: { losses: 1 } },
+      { upsert: true },
+      function(err, result) {
+        if (err) throw err;
+      }
+    );
+    res.json('You Lose');
+    return;
+  }
+});
 
 
 app.listen(3000,  () => {
